9113f395af75b5a4c0f8072df5a5d59a0e28f4f7
davidgonzalezzc <cd-gonzalez@javeriana.edu.co>
2023-04-17 19:08:18 -0500

    running

--- src/main/java/puj/api/userapi/service/UserServiceImp.java
+++ src/main/java/puj/api/userapi/service/UserServiceImp.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import puj.api.userapi.entity.User;
 import puj.api.userapi.repository.UserRepository;
 
+@Service
 public class UserServiceImp implements UserService{
 
     @Autowired
